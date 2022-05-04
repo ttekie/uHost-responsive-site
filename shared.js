ddad6ac20dacec52543693dc386e0b2cc481749e
Tesfalem Tekie <teshanna72016@gmail.com>
2022-05-03 21:59:34 -0500

    modified responsive site

--- shared.js
+++ shared.js
@@ -4,13 +4,17 @@ let planSelectButton = document.querySelectorAll('.plan button');
 let modalNoButton = document.querySelector('.modal__action--negative');
 let toggleButton = document.querySelector('.toggle-button');
 let mobileNav = document.querySelector('.mobile-nav');
+// let ctaButton = document.querySelector('main-nav__item--cta');
 
 for(let i = 0; i < planSelectButton.length; i++) {
    planSelectButton[i].addEventListener('click', function() {
       // backdrop.style.display = 'block';
       // modal.style.display = 'block';
       modal.classList.add('open');
-      backdrop.classList.add('open');
+      backdrop.style.display = "block";
+      setTimeout(function() {
+         backdrop.classList.add('open');
+      }, 10); 
    });
 }
 backdrop.addEventListener('click', function() {
@@ -31,12 +35,28 @@ function closeModal() {
       modal.classList.remove('open');
    }
    backdrop.classList.remove('open');
+   setTimeout(function() {
+      backdrop.style.display = "none"; 
+   }, 200); 
 }
 toggleButton.addEventListener('click', function(){
    // mobileNav.style.display = 'block';
    // backdrop.style.display = 'block';
    mobileNav.classList.add('open');
-   backdrop.classList.add('open');
+   backdrop.style.display = "block";
+   setTimeout(function() {
+      backdrop.classList.add('open');
+   }, 10);
 });
+
+// ctaButton.addEventListener('animationstart', function(event) {
+//    console.log('Animation start', event);
+// });
+// ctaButton.addEventListener('animationend', function(event) {
+//    console.log('Animation end', event);
+// });
+// ctaButton.addEventListener('animationiteration', function(event) {
+//    console.log('Animation iteration', event);
+// });
 
 
